c4dc5b11d4f965337d4d8076e0e30a5a68b33400
ebarnabo <edwinbarnabot@gmail.com>
2024-02-23 14:00:54 -0400

    Changement des icones selon le type

--- db.js
+++
@@ -1,9 +0,0 @@
-// db.js
-const mysql = require('mysql');
-const connection = mysql.createPool({
-    host     : 'localhost',  // Votre hôte
-    user     : 'map',       // Votre utilisateur de base de données
-    password : 'map',   // Votre mot de passe
-    database : 'WBS'         // Le nom de la base de données
-});
-module.exports = connection;
